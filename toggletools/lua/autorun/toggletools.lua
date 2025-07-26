191343471aff44dfce310b9d6e71f12e6dde56c5
Arttu <arttuc@pm.me>
2025-07-26 18:06:19 +0300

    Toggle Tools addon

---
+++ toggletools/lua/autorun/toggletools.lua
@@ -0,0 +1,88 @@
+if CLIENT then
+	CreateClientConVar("toggletools_cmenuicon", "1", true, false, "Should an icon be added to the context menu in sandbox? Requires reload/reconnect.", 0, 1)
+
+	if GetConVar("toggletools_cmenuicon"):GetInt() > 0 then
+		list.Set("DesktopWindows", "physgun-toggle", {
+			title = "Toggle Tools",
+			icon = "icon64/toggletools.png",
+			init = function()
+				LocalPlayer():ConCommand("toggletools")
+			end
+		})
+	end
+
+elseif SERVER then
+	-- List of tools to check for.
+	local tools = {"weapon_physgun", "gmod_camera", "gmod_tool"}
+	-- List of players who have disabled tools.
+	---@type table<number, boolean>
+	local disabledToolsPlayerTable = {}
+
+	---@param plr Player
+	local function hasTools(plr)
+		for _, toolName in ipairs(tools) do
+			if plr:HasWeapon(toolName) then
+				return true
+			end
+		end
+		return false
+	end
+
+	---@param plr Player
+	local function stripTools(plr)
+		local uid = plr:UserID()
+		disabledToolsPlayerTable[uid] = true
+
+		for _, toolName in ipairs(tools) do
+			plr:StripWeapon(toolName)
+		end
+	end
+
+	---@param plr Player
+	local function giveTools(plr)
+		local uid = plr:UserID()
+		disabledToolsPlayerTable[uid] = false
+
+		for _, toolName in ipairs(tools) do
+			plr:Give(toolName, true)
+		end
+	end
+
+	concommand.Add("toggletools", function(plr)
+		if plr == NULL then -- prevent running from dedicated server console
+			print("toggletools can only be executed as a player!")
+			return
+		end
+
+		if GAMEMODE_NAME ~= "sandbox" then
+			print(plr:GetName().." attempted to use toggletools while the server is on a non-sandbox gamemode!")
+			return
+		end
+
+		if hasTools(plr) then
+			stripTools(plr)
+		else
+			giveTools(plr)
+		end
+	end)
+
+	if GAMEMODE_NAME == "sandbox" then
+		gameevent.Listen("player_spawn")
+		hook.Add("player_spawn", "toggletools_memory", function(data)
+			local uid = data.userid
+			if disabledToolsPlayerTable[uid] then
+				timer.Simple(0.05, function()
+					local plr = Player(uid)
+					stripTools(plr)
+				end)
+			end
+		end)
+
+		gameevent.Listen("disconnect")
+		hook.Add("disconnect", "toggletools_disconnect", function(_, _, _, uid)
+			-- unset dtpt value for disconnected uid
+			disabledToolsPlayerTable[uid] = nil
+		end)
+	end
+
+end
